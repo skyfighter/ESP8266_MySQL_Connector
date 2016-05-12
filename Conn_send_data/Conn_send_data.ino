d5cb0b503f6bae2730a662647261bb8048224fa0
Itti Souvapun <itti@yahoo.com>
2016-05-12 13:33:39 +0700

    Update File

---
+++ Conn_send_data/Conn_send_data.ino
@@ -0,0 +1,65 @@
+/* ******Note*******
+    Read more https://github.com/ChuckBell/MySQL_Connector_Arduino/blob/master/extras/MySQL_Connector_Arduino_Reference_Manual.pdf
+
+    Test Access DATABASE first >>> mysql -u**USER** -p**PASSWOED** –h**IP ADDRESS** --port=3306 <<<
+
+  -----------Note for Me-----------------
+  1. sudo nano /etc/mysql/my.cnf --> sudo service mysql restart
+  2. CREATE USER 'USER'@'%' IDENTIFIED BY 'PASSWORD'; or Use PHPMyAdmin to Add user
+  3. Use command 'SELECT user, host FROM mysql.user; and 'SELECT USER(), CURRENT_USER();' read more http://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+  4. Test Use can connect to MySQL Databases
+*/
+#include <ESP8266WiFi.h>
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
+
+byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+IPAddress server_addr(118, 174, 157, 23);
+//IPAddress server_addr(192, 168, 1, 30); // IP of the MySQL *server* here
+char user[] = "conn";              // MySQL user login username
+char pass[] = "tum354527";            // MySQL user login password
+
+char INSERT_SQL[] = "INSERT INTO test_arduino.hello_arduino (message) VALUES('Hello, Arduino!')";
+
+const char* ssid = "iVPN";            //SSID WiFi name
+const char* password = "tum354527";  //Password WiFi
+WiFiClient client;
+MySQL_Connection conn((Client *)&client);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connected to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" ");
+  Serial.println("IP Address");
+  Serial.print(WiFi.localIP());
+
+  Serial.println("Connecting..SQL Server..");
+  if (conn.connect(server_addr, 5106, user, pass)) {
+    delay(1000);
+
+    Serial.println("Recording data...");
+    // Initiate the query class instance
+    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+    // Execute the query
+    cur_mem->execute(INSERT_SQL);
+    // Note: since there are no results, we do not need to read any data
+    // Deleting the cursor also frees up memory used
+    delete cur_mem;
+  }
+  else
+    Serial.println("Connection failed.");
+  conn.close();
+}
+
+void loop() {
+
+}
