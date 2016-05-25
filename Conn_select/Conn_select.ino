9b4edd85f9308fdc56802d3259af0c0fa0a39bd9
Itti Souvapun <itti@yahoo.com>
2016-05-25 14:09:46 +0700

    my ip and ssid

--- Conn_select/Conn_select.ino
+++ Conn_select/Conn_select.ino
@@ -16,13 +16,13 @@
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 IPAddress server_addr(118, 174, 160, 88); // IP of the MySQL *server* here
-char user[] = "***";                      // MySQL user login username
-char pass[] = "***";                      // MySQL user login password
+char user[] = "conn";                      // MySQL user login username
+char pass[] = "tum354527";                      // MySQL user login password
 
 char INSERT_SQL[] = "INSERT INTO test_arduino.hello_arduino (message) VALUES('Hello, Arduino!')";
 
-const char* ssid = "***";           //SSID WiFi name
-const char* password = "***";       //Password WiFi
+const char* ssid = "iVPN";           //SSID WiFi name
+const char* password = "tum354527";       //Password WiFi
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
 
